da0803dc56168b989d0c881c71d90404d55495bb
Hannu Verma <hannuverma78@gmail.com>
2026-05-23 15:49:14 +0530

    render debugging

--- Server/qdrant.py
+++ Server/qdrant.py
@@ -5,6 +5,8 @@ from config.config import QDRANT_URL, QDRANT_API_KEY
 client = QdrantClient(
     url=QDRANT_URL, 
     api_key=QDRANT_API_KEY,
+    timeout=10,
+    check_compatibility=False
 )
 
 # print(qdrant_client.get_collections())
